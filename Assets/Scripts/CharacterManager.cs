e5cc70cb186ef36f263083e98fd5d146482a070a
sravan <sravan.kumar1691@gmail.com>
2022-02-04 07:05:52 +0000

    Added assets and animation panel

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -3,35 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class CharacterObject
+{
+    public List<Sprite> chest = new List<Sprite>();
+    public Image chestImage;
+    public GameObject chestButton;
+    public int chestCount = 0;
+}
+
 public class CharacterManager : MonoBehaviour
 {
-    public Image chestImage;
+    public GameObject customizePanel;
+    public GameObject characterPanel;
+    public GameObject animatePanel;
+
+    public CharacterObject chestObject= new CharacterObject();
+
+    /* default Chest Objects
     public List<Sprite> chest = new List<Sprite>();
-    public int chestCount=0;
+    public Image chestImage;
     public GameObject chestButton;
-  
+    public int chestCount=0;
+    */
+
+    //public CharacterObject headObject= new CharacterObject();
+    
     void Start()
     {
-        chestImage.sprite = chest[chestCount];
+        customizePanel.SetActive(true);
+        characterPanel.SetActive(false);
+
+       chestObject.chestImage.sprite = chestObject.chest[chestObject.chestCount];
     }
 
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           CreateCharacter();   
+        }
         
     }
 
     public void CustomizeChest()
     {
-        if (chestCount == chest.Count-1)
+        if (chestObject.chestCount == chestObject.chest.Count-1)
         {
-            chestCount = 0;
+            chestObject.chestCount = 0;
         }
         else
         {
-            chestCount++;
+            chestObject.chestCount++;
         }
 
-        chestImage.sprite= chest[chestCount];
+        chestObject.chestImage.sprite= chestObject.chest[chestObject.chestCount];
+    }
+
+    public void CreateCharacter()
+    {
+        Debug.Log(chestObject.chestCount);
+
+        animatePanel.SetActive(true);
+        characterPanel.SetActive(true);
+
+        customizePanel.SetActive(false);
+
+        characterPanel.gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = chestObject.chest[chestObject.chestCount];
+    }
+
+    public void BackCustomize()
+    {
+        animatePanel.SetActive(true);
+        customizePanel.SetActive(true);
+
+        characterPanel.SetActive(!customizePanel.active);
+    }
+
+
+    public void EnablePanel()
+    {
+
     }
 }
+
